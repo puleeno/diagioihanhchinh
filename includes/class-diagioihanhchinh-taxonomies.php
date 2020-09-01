c30144e9a2b2753a1144d0ec73800f1d20e90782
Puleeno Nguyen <puleeno@gmail.com>
2020-09-01 21:43:31 +0700

    Fix diagioihanhchinh command

--- includes/class-diagioihanhchinh-taxonomies.php
+++ includes/class-diagioihanhchinh-taxonomies.php
@@ -5,6 +5,14 @@ class Diagioihanhchinh_Taxonomies {
 	}
 
 	public function register_locations() {
+		$applied_post_types = apply_filters( 'diagioihanhchinh_post_types', array() );
+		if ( empty( $applied_post_types ) ) {
+			Logger::get( 'diagioihanhchinh' )->debug(
+				'You must set post types to register locations',
+				$applied_post_types
+			);
+			return;
+		}
 	}
 }
 
--- includes/class-diagioihanhchinh.php
+++ includes/class-diagioihanhchinh.php
@@ -1,4 +1,8 @@
 <?php
+use Monolog\Logger as Monolog;
+use Monolog\Handler\StreamHandler;
+use Ramphor\Logger\Logger;
+
 class Diagioihanhchinh {
 	protected static $instance;
 
@@ -14,6 +18,21 @@ class Diagioihanhchinh {
 		$this->init_hooks();
 	}
 
+	public function setup_logger() {
+		$log           = new Monolog( 'DIAGIOIHANHCHINH' );
+		$stream_handle = new StreamHandler(
+			apply_filters( 'diagioihanhchinh_logs_path', sprintf( '%s/diagioihanhchinh.log', WP_CONTENT_DIR ) ),
+			Monolog::DEBUG
+		);
+
+		// Push stream handler to logger
+		$log->pushHandler( $stream_handle );
+
+		// Register diagioihanhchinh log to Ramphor Logger
+		$logger = Logger::instance();
+		$logger->registerLogger( 'diagioihanhchinh', $log );
+	}
+
 	protected function includes() {
 		require_once dirname( __FILE__ ) . '/class-diagioihanhchinh-common.php';
 		require_once dirname( __FILE__ ) . '/class-diagioihanhchinh-taxonomies.php';
@@ -23,5 +42,7 @@ class Diagioihanhchinh {
 
 	protected function init_hooks() {
 		register_activation_hook( DIAGIOIHANHCHINH_PLUGIN_FILE, array( Diagioihanhchinh_Install::class, 'active' ) );
+
+		add_action( 'plugins_loaded', array( $this, 'setup_logger' ) );
 	}
 }
